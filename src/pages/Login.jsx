4c518243592bb615240a71fbeb5d13a7934080b0
Rakitha <tharupathir@gmail.com>
2024-08-01 23:17:58 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,38 @@
+import axios from "axios";
 import { useState } from "react";
 
 const Login = () => {
   const [number, setNumber] = useState('');
+  const [ok,setOk] = useState(false);
 
   const handleSetNumber = (e) => {
     setNumber(e.target.value.replace(/\D/,''));
   }
   const handleEnter =(e)=>{
     e.preventDefault();
-    console.log(number)
+    console.log(number);
+    
+    axios.post(ok?'http://localhost:3000/login/verify':'http://localhost:3000/login/',ok?{
+      otp:number,
+    }:{pnumber:number})
+    .then((res)=>{
+      if(ok){
+        let session = res.data;
+        localStorage.setItem("session",session)
+      }
+      else{
+        if(res.status == 200){
+          setOk(true);
+        }
+        console.log(ok)
+      }
+     
+    })
   }
   const handleEnterKey = (e)=>{
     e.key=='Enter'?handleEnter(e):null
   }
+  
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
@@ -20,7 +40,7 @@ const Login = () => {
         <h1 className="text-3xl font-bold text-blue-600 mb-4 text-center">Login</h1>
         <hr className="mt-3 mb-5"/>
         <div className="text-left">
-          <label htmlFor="number" className="block pb-2 text-xl">Number</label>
+          <label htmlFor="number" className="block pb-2 text-xl">{ok ? 'OTP' : 'Phone Number'}</label>
           <input
             type="text"
             id="number"
